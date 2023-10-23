3603c7da1b8f0c835f1f46b7b8ce0da8c7fbdec0
Gaëtan <blaxoujunior@gmail.com>
2023-10-23 19:35:44 +0200

    sorted-twice

--- crates/nu-engine/src/column.rs
+++ crates/nu-engine/src/column.rs
@@ -2,17 +2,33 @@ use indexmap::IndexSet;
 use nu_protocol::Value;
 use std::collections::HashSet;
 
-pub fn get_columns(input: &[Value]) -> Vec<String> {
-    let mut set: IndexSet<&String, ahash::RandomState> = IndexSet::default();
+// Arbitrary threshold for when to use a hashset instead of a vector
+const HASHSET_THRESHOLD: usize = 100;
 
+pub fn get_columns(input: &[Value]) -> Vec<String> {
+    let mut set: Vec<(usize, &String)> = vec![];
+
+    let mut index = 0;
     for item in input {
-        let Value::Record { val, .. } = item else {
-            return vec![];
+        if let Value::Record { val, .. } = item {
+            for col in &val.cols {
+                set.push((index, col));
+                index += 1;
+            }
         };
-        set.extend(&val.cols);
     }
 
-    set.iter().map(|x| x.to_string()).collect()
+    // if set.len() < HASHSET_THRESHOLD {
+    set.sort_by(|(_, s0), (_, s1)| s0.cmp(s1));
+    set.dedup();
+    set.sort_by(|(i0, _), (i1, _)| i0.cmp(i1));
+    set.iter().map(|(_, s)| s.to_string()).collect()
+    // } else {
+    //     IndexSet::<(usize, &String)>::from_iter(set)
+    //         .iter()
+    //         .map(|(_, x)| x.to_string())
+    //         .collect()
+    // }
 }
 
 // If a column doesn't exist in the input, return it.
