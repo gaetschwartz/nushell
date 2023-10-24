2d8cbac11b0adfe0c4d35e17f96d393564a9f73e
Gaëtan <blaxoujunior@gmail.com>
2023-10-24 08:22:40 +0200

    use threshold > 200

--- crates/nu-engine/src/column.rs
+++ crates/nu-engine/src/column.rs
@@ -3,32 +3,42 @@ use nu_protocol::Value;
 use std::collections::HashSet;
 
 // Arbitrary threshold for when to use a hashset instead of a vector
-const HASHSET_THRESHOLD: usize = 100;
+const HASHSET_THRESHOLD: usize = 200;
 
 pub fn get_columns(input: &[Value]) -> Vec<String> {
-    let mut set: Vec<(usize, &String)> = vec![];
-
-    let mut index = 0;
-    for item in input {
-        if let Value::Record { val, .. } = item {
-            for col in &val.cols {
-                set.push((index, col));
-                index += 1;
+    let tota_cols = {
+        let mut total_cols = 0;
+        for item in input {
+            if let Value::Record { val, .. } = item {
+                total_cols += val.cols.len();
             }
-        };
-    }
+        }
+        total_cols
+    };
 
-    // if set.len() < HASHSET_THRESHOLD {
-    set.sort_by(|(_, s0), (_, s1)| s0.cmp(s1));
-    set.dedup();
-    set.sort_by(|(i0, _), (i1, _)| i0.cmp(i1));
-    set.iter().map(|(_, s)| s.to_string()).collect()
-    // } else {
-    //     IndexSet::<(usize, &String)>::from_iter(set)
-    //         .iter()
-    //         .map(|(_, x)| x.to_string())
-    //         .collect()
-    // }
+    if tota_cols > HASHSET_THRESHOLD {
+        let mut set: IndexSet<String> = IndexSet::new();
+        for item in input {
+            if let Value::Record { val, .. } = item {
+                for col in &val.cols {
+                    set.insert(col.clone());
+                }
+            };
+        }
+        set.into_iter().collect()
+    } else {
+        let mut set = vec![];
+        for item in input {
+            if let Value::Record { val, .. } = item {
+                for col in &val.cols {
+                    if !set.contains(col) {
+                        set.push(col.clone());
+                    }
+                }
+            };
+        }
+        set
+    }
 }
 
 // If a column doesn't exist in the input, return it.
